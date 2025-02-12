7640c034a9ff21ba095a4cb69e4ffc36eacc5447
Johan Saenz Ramos <47581366+jfsaenzr@users.noreply.github.com>
2025-02-12 09:49:32 -0500

    Update worker.js
    Ajustes *

--- worker.js
+++ worker.js
@@ -1,4 +1,5 @@
-import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.46/+esm"
+//import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm"
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.78/+esm"
 
 const handler = new WebWorkerMLCEngineHandler()
 
